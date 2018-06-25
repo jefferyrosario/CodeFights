bfe12a85e09820a900e9b0dbac50e196ed93e964
Jeffery Rosario <jeffrosario92@gmail.com>
2018-06-25 13:12:23 -0400

    Add files via upload

---
+++ databases/monthlyScholarships.sql
@@ -0,0 +1,5 @@
+CREATE PROCEDURE monthlyScholarships()
+BEGIN
+SELECT id, (scholarship/12) scholarship
+FROM scholarships;
+END
---
+++ databases/volleyballResults.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE volleyballResults()
+BEGIN
+SELECT name, country, scored, missed, wins
+FROM results
+ORDER BY wins
+ASC;
+END
